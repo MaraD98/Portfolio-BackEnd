b488bb94e7fd9f3c08f4cfb8e05200d084119f45
MaraD98 <maradelgadillo2020@gmail.com>
2022-09-12 15:28:56 -0300

    Añadi componentes de experiencia y educacion

--- src/main/java/com/myportfolio/mcd/Controller/PersonaController.java
+++ src/main/java/com/myportfolio/mcd/Controller/PersonaController.java
@@ -59,6 +59,7 @@ public class PersonaController {
     
     @GetMapping("/personas/traer/perfil")
     public Persona findpersona(){
-        return ipersonaService.findPersona((long)1);}
+        return ipersonaService.findPersona((long)1);
+    }
     
 }
