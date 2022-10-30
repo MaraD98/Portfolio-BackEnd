a79595d8706ce679c1477ff8b6ad7a5a80c98f66
MaraD98 <maradelgadillo2020@gmail.com>
2022-10-30 20:34:25 -0300

    Haciendo el routing del back

--- src/main/java/com/myportfolio/mcd/Controller/CExperiencia.java
+++ src/main/java/com/myportfolio/mcd/Controller/CExperiencia.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/explab")
+@CrossOrigin(origins = "https://frontendmcd.web.app")
 public class CExperiencia {
 
     @Autowired
